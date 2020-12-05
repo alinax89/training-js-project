31e8e1a7b26fb03eadbee6a3e25d952e7cc7f045
Andrei <aalisenak@gmail.com>
2020-12-05 16:45:01 +0300

    finish resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,26 +3,36 @@ const CODES = {
     Z: 90
 }
 
-function toCell() {
+function toCell(_, col) {
     return `
-    <div class="cell" contenteditable>NEW</div>
+    <div class="cell" contenteditable data-col="${col}">NEW</div>
     `
 }
 
-function toColumn(col) {
+function toColumn(col, index) {
     return `
-    <div class="column">${col}</div>
+    <div class="column" data-type="resizable" data-col="${index}">
+        ${col}    
+        <div class="col-resize" data-resize="col"></div>
+    </div>
     `
 }
 
 function createRow(index, content) {
+    const resize = index
+        ? '<div class="row-resize" data-resize="row"></div>'
+        : ''
     return `
-    <div class="row">
-        <div class="row-info">${index ? index : ''}</div>
+    <div class="row" data-type="resizable">
+        <div class="row-info">
+            ${index ? index : ''}
+            ${resize}
+        </div>
         <div class="row-data">${content}</div>
     </div>
     `
 }
+
 function toChar(_, index) {
     return String.fromCharCode(CODES.A + index)
 }
